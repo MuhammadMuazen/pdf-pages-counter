c90cd670a0b0229429aea7f6cfdb35db31637885
MuhammadMuazen <muhammadmuazen1@gmail.com>
2025-02-10 02:58:57 -1200

    adding book number to the print

--- count.py
+++ count.py
@@ -5,9 +5,10 @@
 import os
 import PyPDF2
 
-folder_path = r"C:\Users\User\Folder" #Change This
+folder_path = r"C:\Users\muhammad\Desktop\files\Books-To-Print" #Change This
 number_of_pages = []
 pages_sum = 0
+book_number = 1
 # Loop through all files in the folder
 for file_name in os.listdir(folder_path):
     if file_name.endswith(".pdf"):
@@ -16,8 +17,9 @@ for file_name in os.listdir(folder_path):
             pdf_reader = PyPDF2.PdfReader(pdf_file)
             num_pages = len(pdf_reader.pages)
             number_of_pages.append(num_pages)
-            print(f"{file_name}: {num_pages} pages")
+            print(f"[{book_number}] {file_name}: {num_pages} pages")
+        book_number += 1
 
 for i in number_of_pages:
     pages_sum += i
-print(f'The Full Pages Sum is {pages_sum}')
+print(f'[*] The Full Pages Sum is {pages_sum}')
